c281a5ede0fb9538e7f5d30f1c8661111e30ea9e
SatisKia <satis@msd.biglobe.ne.jp>
2024-04-05 08:11:05 +0900

    fix

--- data/data.bat
+++ data/data.bat
@@ -7,4 +7,6 @@ certutil -f -encode sample2.png tmp\2.txt
 type tmp\1.txt | dataurl png > out\sample1.txt
 type tmp\2.txt | dataurl png > out\sample2.txt
 
+mqo2gl ground.mqo 5 1 texture.txt 99999 > out\ground.txt
+
 pause
