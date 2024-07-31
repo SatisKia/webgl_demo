f7d8de04f0eb09f6ff52ceaae17b976318a888e1
SatisKia <satis@msd.biglobe.ne.jp>
2024-07-31 23:19:39 +0900

    fix

--- d2js_gl.debug.js
+++ d2js_gl.debug.js
@@ -697,14 +697,72 @@ _GLPrimitive.prototype = {
 		return this._trans;
 	}
 };
-function _GLShader( vsSource, fsSource ){
+function _GLShader( vsSource, fsSource, useVars ){
 	this._program = createShaderProgram( vsSource, fsSource );
+	if( (useVars != undefined) && (useVars == true) ){
+		var i, j;
+		var tmp, tmp2, tmp3, tmp4, tmp5;
+		var type;
+		var top, end;
+		var name;
+		this.vars = [];
+		for( j = 0; j < 2; j++ ){
+			tmp = "" + ((j == 0) ? vsSource : fsSource);
+			for( i = 0; i < 2; i++ ){
+				type = ((i == 0) ? "attribute" : "uniform");
+				while( true ){
+					if( (top = tmp.indexOf( type )) >= 0 ){
+						tmp2 = tmp.substring( top );
+						if( (end = tmp2.indexOf( ";" )) >= 0 ){
+							name = "";
+							tmp3 = tmp2.substring( 0, end );
+							tmp4 = tmp3.split( "\t" );
+							if( tmp4.length > 0 ){
+								tmp5 = tmp4[tmp4.length - 1].split( " " );
+								if( tmp5.length > 0 ){
+									name = tmp5[tmp5.length - 1];
+								} else {
+									name = tmp4[tmp4.length - 1];
+								}
+							} else {
+								tmp4 = tmp3.split( " " );
+								if( tmp4.length > 0 ){
+									name = tmp4[tmp4.length - 1];
+								}
+							}
+							if( name.length > 0 ){
+								if( i == 0 ){
+									this.vars[name] = _gl.getAttribLocation( this._program, name );
+								} else {
+									this.vars[name] = _gl.getUniformLocation( this._program, name );
+								}
+							}
+							if( end + 1 >= tmp.length ){
+								break;
+							}
+							tmp = tmp.substring( end + 1 );
+						} else {
+							break;
+						}
+					} else {
+						break;
+					}
+				}
+			}
+		}
+	}
 }
 _GLShader.prototype = {
 	attrib : function( name ){
+		if( this.vars != undefined ){
+			return this.vars[name];
+		}
 		return _gl.getAttribLocation( this._program, name );
 	},
 	uniform : function( name ){
+		if( this.vars != undefined ){
+			return this.vars[name];
+		}
 		return _gl.getUniformLocation( this._program, name );
 	},
 	use : function(){
